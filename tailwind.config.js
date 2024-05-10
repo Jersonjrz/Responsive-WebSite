0dc8edd669e340a94bd20d4501b260b663fc3570
jersonjrz <jersond45@gmail.com>
2024-05-10 00:20:08 -0500

    add until card 1 price

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const { transform } = require('sucrase');
+const plugin = require('tailwindcss/plugin');
+
+const rotateY = plugin(function ({ addUtilities }) {
+  addUtilities ({
+    '.rotate-y-180': {
+      transform: "rotateY(180deg)"
+    },
+    '.-rotate-y-180': {
+      transform: "rotateY(-180deg)"
+    }
+  })
+})
+
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
@@ -21,6 +36,6 @@ module.exports = {
       }
     }
   },
-  plugins: [],
+  plugins: [rotateY],
 }
 
